b3cc95b795e2f87ab359be4170d099aac535668a
jmontoyajr <82304971+jmontoyajr@users.noreply.github.com>
2021-07-22 00:07:38 -0700

    data security, validation and ethics

--- index.js
+++ index.js
@@ -15,11 +15,29 @@ const express = require('express');
     bodyParser = require('body-parser');
     uuid = require('uuid');
 
+const { check, validationResult } = require('express-validator');
+
 const app = express();
 
 // Body Parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Require CORS
+const cors = require('cors');
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1){ // if specific origin isn't found on the list of allowed origins
+            let message = 'The CORS policy for this application doesn\'t allow access from origin' + origin;
+            return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+}
+    
+}));
 
 // Require Auth.js
 let auth = require('./auth')(app);
@@ -173,29 +191,48 @@ app.get('/users/:Username', (req, res) => {
     Email: String,
     Birthday: Date
 }*/
-app.post('/users', (req, res) => {
-    Users.findOne({ Username: req.body.Username })
+app.post('/users',
+    // Validation logic request
+    // you can either use a chain of methods like .not().isEmpty()
+    // which means "opposite of isEmpty" in plain English "is not empty"
+    // or use .isLength({min: 5}) which means
+    // minimum value of 5 characters are only allowed
+    [
+      check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+      check('Password', 'Password is required.').not().isEmpty(),
+      check('Email', 'Email does not appear to be valid.').isEmail()
+    ], (req, res) => {
+    
+    // check the validation object for errors
+    let errors = validationResult(req);
+    
+    if(!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array()});
+    }
+    
+    let hashedPassword = Users.hashPassword(req.body.Password);
+    Users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
         .then((user) => {
-            if (user) {
+            if (user) { //If the user is found, send a response that it already exists
                 return res.status(400).send(req.body.Username + 'already exists.');
             } else {
               Users
                 .create({
                     Username: req.body.Username,
-                    Password: req.body.Password,
+                    Password: hashedPassword,
                     Email: req.body.Email,
                     Birthday: req.body.Birthday
                 })
-                .then((user) =>{res.staus(201).json(user) })
+                .then((user) => { res.staus(201).json(user) })
                 .catch((error) => {
                     console.error(error);
                     res.status(500).send('Error: ' + error);
-                })
+                });
             }
         })
-    .catch((error) => {
-        console.error(error);
-        res.status(500).send('Error: ' + error);
+        .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
     });
 });
 
@@ -282,6 +319,7 @@ app.use((err, req, res, next) => {
 });
 
 // listen for requests
-app.listen(8080, () => {
-  console.log('Your app is listening on port 8080.');
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0',() => {
+    console.log('Listening on Port ' + port);
 });
